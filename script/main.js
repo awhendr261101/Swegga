bb966d4da0244ff795b0c358f1b651e0a06c1114
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-05-31 17:29:39 +0200

    fixed bug

--- script/main.js
+++ script/main.js
@@ -6,15 +6,17 @@ let num2;
 document.querySelectorAll(".entry").forEach((value,index) => value.addEventListener("change", ()=>{
     if(index == 0){
         num1 = eval(value.value * 150.95)
-        list[index].textContent = (eval(value.value * 150.95)).toFixed(2)
+        list[index].textContent = `R${(eval(value.value * 150.95)).toFixed(2)}`
     } else {
         num2 = eval(value.value * 180.95)
-        list[index].textContent = (eval(value.value * 180.95)).toFixed(2)
+        list[index].textContent = `R${(eval(value.value * 180.95)).toFixed(2)}`
     }
 }))
 
 
 document.querySelector(".total").addEventListener("click", ()=>{
-    list[2].textContent = (num1 + num2).toFixed(2)
+    list[2].textContent = `R${(num1 + num2).toFixed(2)}`
 })
+
+
 
